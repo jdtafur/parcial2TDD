e3389ec85ce63792a03b16a557768386cb622f92
Juan D <juandavidtafurrangel@gmail.com>
2021-05-03 18:29:36 -0500

    pruebas de dominio

---
+++ Inventario.Core.Domain.Test/Compuesto.Test.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Inventario.Core.Domain.Test
+{
+    public class Compuesto_Test
+    {
+        /*
+           Escenario: Retirar cantidad igual a cero 
+           H1: COMO USUARIO QUIERO REGISTRAR LA SALIDA PRODUCTOS 
+
+           Criterio de Aceptación:
+           La cantidad de la salida de debe ser mayor a 0
+           
+           Ejemplo
+           Dado El cliente crea un producto compuesto
+           Código 10001, Nombre “perro sencillo”, precio 5000, costo debe ser 3000 calculado,
+           
+           ingredientes:
+           1 salchicha
+           1 lamino de queso
+           1 pan perro
+           
+           Cuando Va a retirar 0 cantidad de dicho producto    
+
+           Entonces El sistema presentará el mensaje. "la cantidad a retirar debe ser mayor a cero"
+       */
+
+        [Test]
+        public void ExtractCeroProductCompoundFailTest()
+        {
+            
+            //Preparar
+            var perro = new Compuesto("10001", "perro sencillo", 5000);
+            
+            //Acción
+            //se podra ingresar la cantidad que necesita de cada ingrediente
+            var salchicha = new Simple( "salchicha", 1000, 1);
+            var laminaQueso = new Simple("lamina de queso", 1000, 1000);
+            var panPerro = new Simple("pan perro", 1000, 1);
+            
+            List<Simple> ingredientes = new List<Simple>();
+            ingredientes.Add(salchicha);
+            ingredientes.Add(laminaQueso);
+            ingredientes.Add(panPerro);
+            
+            perro.RegistroIngredientes(ingredientes);
+
+            var resultado = perro.Retiro(0);
+            
+            //Verificación
+            Assert.AreEqual("la cantidad a retirar debe ser mayor a cero", resultado);
+        }
+        
+        /*
+           Escenario: Retirar cantidad igual A 1
+           H1: COMO USUARIO QUIERO REGISTRAR LA SALIDA PRODUCTOS 
+
+           Criterio de Aceptación:
+           En caso de un producto compuesto la cantidad de la salida se le disminuirá a la cantidad existente de cada uno de su ingrediente
+           
+           Ejemplo
+           Dado El cliente crea un producto compuesto
+           Código 10001, Nombre “perro sencillo”, precio 5000, costo debe ser 3000 calculado,
+           
+           ingredientes:
+           1 salchicha
+           1 lamino de queso
+           1 pan perro
+           
+           Cuando Va a retirar 1 cantidad de perro sencillo  
+
+           Entonces El sistema presentará el mensaje. "Se realizó el retiro de 1 producto(s) de tipo compuesto perro sencillo con un costo de 3000 y un precio de 5000"
+       */
+
+        [Test]
+        public void ExtractOneProductCompoundSuccesTest()
+        {
+            //Preparar
+            var perro = new Compuesto("10001", "perro sencillo", 5000);
+
+            //Acción
+            //se podra ingresar la cantidad que necesita de cada ingrediente
+            var Isalchicha = new Simple( "salchicha", 1000, 1);
+            var IlaminaQueso = new Simple("lamina de queso", 1000, 1);
+            var IpanPerro = new Simple("pan perro", 1000, 1);
+
+            List<Simple> ingredientes = new List<Simple>();
+            ingredientes.Add(Isalchicha);
+            ingredientes.Add(IlaminaQueso);
+            ingredientes.Add(IpanPerro);
+            
+            perro.RegistroIngredientes(ingredientes);
+
+            var resultado = perro.Retiro(1);
+            
+            //Verificación
+            Assert.AreEqual("Se realizó el retiro de 1 producto(s) de tipo compuesto perro sencillo con un costo de 3000 y un precio de 5000", resultado);
+        }
+    }
+}
---
+++ Inventario.Core.Domain.Test/Simple.Test.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Inventario.Core.Domain.Test
+{
+    public class Simple_Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        
+        /*
+           Escenario: registrar cantidad igual a cero 
+           H1: COMO USUARIO QUIERO REGISTRAR LA ENTRADA PRODUCTOS 
+
+           Criterio de Aceptación:
+           La cantidad de la entrada de debe ser mayor a 0
+
+           Ejemplo
+           Dado El cliente crea un producto simple
+           Código 10001, Nombre “salchicha”, precio 5000, costo 2000, ingrediente
+
+           Cuando Va a registrar 0 cantidad de dicho producto    
+
+           Entonces El sistema presentará el mensaje. “la cantidad a registrar debe ser mayor a 0 y usted intentó registrar 0 unidades”
+       */
+
+        [Test]
+        public void RegisterProductSimpleFailTest()
+        {
+            //Preparar
+            var salchicha = new ProductoSimple("10001", "salchicha", 1000, 1000, "ingrediente");
+            
+            //Acción
+            var resultado = salchicha.Registro(0);
+            
+            //Verificación
+            Assert.AreEqual("la cantidad a registrar debe ser mayor a 0 y usted intentó registrar 0 unidades", resultado);
+        }
+        
+        /*
+           Escenario: registrar cantidad diferente a cero 
+           H1: COMO USUARIO QUIERO REGISTRAR LA ENTRADA PRODUCTOS 
+
+           Criterio de Aceptación:
+           La cantidad de la entrada de debe ser mayor a 0
+
+           Ejemplo
+           Dado El cliente crea un producto simple
+           Código 10001, Nombre “salchicha”, precio 5000, costo 2000, ingrediente
+
+           Cuando Va a registrar 10 cantidad de dicho producto    
+
+           Entonces El sistema presentará el mensaje. “Producto agregado, ahora hay 10 unidad(es) del producto salchicha en inventario”
+       */
+
+        [Test]
+        public void RegisterProductSimpleSuccesTest()
+        {
+            //Preparar
+            var salchicha = new ProductoSimple("10001", "salchicha", 1000, 1000, "ingrediente");
+            
+            //Acción
+            var resultado = salchicha.Registro(10);
+            
+            //Verificación
+            Assert.AreEqual("Producto agregado, ahora hay 10 unidad(es) del producto salchicha en inventario", resultado);
+        }
+        
+        /*
+           Escenario: Retirar cantidad igual a cero 
+           H1: COMO USUARIO QUIERO REGISTRAR LA SALIDA PRODUCTOS 
+
+           Criterio de Aceptación:
+           La cantidad de la salida de debe ser mayor a 0
+           
+           Ejemplo
+           Dado El cliente crea un producto simple
+           Código 10001, Nombre “salchicha”, precio 5000, costo 2000, ingrediente
+           registra 10 cantidad del producto 
+           
+           Cuando Va a retirar 0 cantidad de dicho producto    
+
+           Entonces El sistema presentará el mensaje. "la cantidad a retirar debe ser mayor a 0 y usted intentó retirar 0 unidades"
+       */
+
+        [Test]
+        public void ExtractCeroProductSimpleFailTest()
+        {
+            var salchicha = new ProductoSimple("10001", "salchicha", 1000, 1000, "ingrediente");
+            salchicha.Registro(10);
+            
+            var resultado = salchicha.Retiro(0);
+            
+            //Verificación
+            Assert.AreEqual("la cantidad a retirar debe ser mayor a 0 y usted intentó retirar 0 unidades", resultado);
+        }
+        
+        /*
+           Escenario: Retirar cantidad mayor a la cantidad en inventario
+           H1: COMO USUARIO QUIERO REGISTRAR LA SALIDA PRODUCTOS 
+
+           Ejemplo
+           Dado El cliente crea un producto simple
+           Código 10001, Nombre “salchicha”, precio 5000, costo 2000, ingrediente
+           registra 10 cantidad del producto 
+           
+           Cuando Va a retirar 15 cantidad de dicho producto    
+
+           Entonces El sistema presentará el mensaje. “lo sentimos, solo hay 10 unidades disponibles en inventario”
+       */
+
+        [Test]
+        public void ExtractBigProductSimpleFailTest()
+        {
+            var salchicha = new ProductoSimple("10001", "salchicha", 1000, 1000, "ingrediente");
+            salchicha.Registro(10);
+            
+            var resultado = salchicha.Retiro(15);
+            
+            //Verificación
+            Assert.AreEqual("lo sentimos, solo hay 10 unidad(es) disponible(s) en inventario", resultado);
+        }
+        
+        /*
+           Escenario: Retirar cantidad correcta en inventario
+           H1: COMO USUARIO QUIERO REGISTRAR LA SALIDA PRODUCTOS 
+
+           Criterio de Aceptación:
+           En caso de un producto sencillo la cantidad de la salida se le disminuirá a la cantidad existente del producto.
+
+           Ejemplo
+           Dado El cliente crea un producto simple
+           Código 10001, Nombre “salchicha”, precio 5000, costo 2000, ingrediente
+           registra 20 cantidad del producto 
+           
+           Cuando Va a retirar 5 cantidad de dicho producto    
+
+           Entonces El sistema presentará el mensaje. “Cantidad de producto actualizado, ahora hay 15 unidades del producto salchicha en inventario"
+       */
+
+        [Test]
+        public void ExtractProductSimpleSuccesTest()
+        {
+     
+            var salchicha = new ProductoSimple("10001", "salchicha", 1000, 1000, "ingrediente");
+            salchicha.Registro(20);
+            
+            var resultado = salchicha.Retiro(5);
+            
+            //Verificación
+            Assert.AreEqual("Cantidad de producto actualizado, ahora hay 15 unidades del producto salchicha en inventario", resultado);
+        }
+        
+    }
+}
